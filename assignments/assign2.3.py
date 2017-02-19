fad9632b73993e8bf04fe4a142885420c1603b85
YidiZhu <zhuyidiwow@gmail.com>
2017-02-19 20:36:15 +0800

    Initial commit

---
+++ assignments/assign2.3.py
@@ -0,0 +1,9 @@
+# This first line is provided for you
+
+hours = raw_input("Enter Hours:")
+hours = float(hours)
+
+rate = raw_input("Enter Rate:")
+rate = float(rate)
+
+print hours * rate
---
+++ playground.py
@@ -0,0 +1,6 @@
+inp = raw_input('Europe floor?')
+usf = int(inp) + 1
+print 'US Floor', usf
+# comments
+
+
